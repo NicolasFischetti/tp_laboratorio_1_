42be5d888d6df7a48d3f8435c63ddebf4e259552
unknown <Nicolas Fischetti>
2018-09-03 21:10:08 -0300

    Tp 1 terminado

---
+++ TP_1_Cascara/funciones.h
@@ -0,0 +1,13 @@
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+
+
+
+#endif // FUNCIONES_H_INCLUDED
+
+int sumar(int x, int y, int *resultado);
+int restar(int x, int y, int *resultado);
+int dividir(int x, int y, float *resultadoDivision);
+int multiplicar(int x, int y, int *resultado);
+int factoriar(int x, int *resultado);
+
